6b0642e8df7d4f6fca5e01d5e71541c34c49d8da
Umut Yiğit <yigitumut2002@gmail.com>
2026-08-08 12:53:27 +0300

    chore: configure application environment

--- src/test/java/com/yigit/wordlyai/WordlyAiBackendApplicationTests.java
+++ src/test/java/com/yigit/wordlyai/WordlyAiBackendApplicationTests.java
@@ -2,8 +2,10 @@ package com.yigit.wordlyai;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class WordlyAiBackendApplicationTests {
 
     @Test
