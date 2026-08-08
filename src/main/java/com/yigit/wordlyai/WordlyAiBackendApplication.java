ae38710d8d88e3d58060fc17e0dde095214f2c8d
Umut Yiğit <yigitumut2002@gmail.com>
2026-08-08 12:50:36 +0300

    chore: initialize WordlyAI backend project

---
+++ src/main/java/com/yigit/wordlyai/WordlyAiBackendApplication.java
@@ -0,0 +1,12 @@
+package com.yigit.wordlyai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordlyAiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WordlyAiBackendApplication.class, args);
+    }
+}
---
+++ src/test/java/com/yigit/wordlyai/WordlyAiBackendApplicationTests.java
@@ -0,0 +1,12 @@
+package com.yigit.wordlyai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordlyAiBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
